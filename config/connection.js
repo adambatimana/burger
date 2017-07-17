f0e954ea664d78bc1b1c091caceecccff36fef6f
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-07-17 14:25:06 -0400

    connectionjs

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+let mysql = require("mysql");
+
+let connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password: "root",
+    database: "burgers_db"
+});
+
+connection.connect(function(err){
+  if (err) {
+      console.error("ERROR CONNECTING: " + err.stack);
+      return;
+  }
+      console.log("CONNECTED AS ID: " + connection.threadId);
+});
+
+
+// module.exports = connection;
